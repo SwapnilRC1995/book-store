c23bb174f1d3f1ea544f9e732a458beb9494695e
swapnilroychowdhury <sroychowdhury@sportsbiz.com>
2024-01-23 18:10:11 -0500

    Code cleanup

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import {bookSlice} from "@/redux/bookSlice";
 import reducer from "@/app/reducers/bookReducers";
 
 export const makeStore = () => {
@@ -8,8 +7,6 @@ export const makeStore = () => {
     })
 }
 
-// Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
